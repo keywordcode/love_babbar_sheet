8607469e8e1e8ff94a4172f07b89169c723c7a38
keywordcode <81748893+keywordcode@users.noreply.github.com>
2021-04-01 15:34:24 +0530

    Create Reverse an Array.cpp

---
+++ Reverse an Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    int begin=0;
+	    int end=n-1;
+	    while(begin<end)
+	    {
+	        swap(a[begin],a[end]);
+	        begin++;
+	        end--;
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        cout<<a[i]<<" ";
+	    }
+	    cout<<endl;
+	}
+}
